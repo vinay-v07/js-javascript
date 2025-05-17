e3942322346f18704025efdd28f39a967944c4ad
vinay verma <itxvinayverma@gmail.com>
2025-05-17 06:04:12 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version.
+
+//alert("hello") // we are using nodeJs, not browser.
+
+let name = "vinay"   //string
+let age = 22         //number
+let islogedIn = false   //boolean
+let state;  // undefined
+
+// number => 2 to power 53
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+
+// object
+
+
+console.log(typeof "vinay"); //string
+console.log(typeof age);     //number
+
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
+
