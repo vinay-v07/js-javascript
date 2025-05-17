02749a61b2500bb3eb957198ef2f306ffef3de8c
vinay verma <itxvinayverma@gmail.com>
2025-05-17 08:48:32 +0000

    comparision in values

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -1,9 +1,28 @@
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 <= 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);       //true
+console.log("02" > 1);      //true
+
+console.log(null > 0);      //false
+console.log(null ==0);      //false
+console.log(null < 0);      //false
+
+console.log(null >= 0);     //true  : here 'null' convert to 'zero'.
+console.log(null <= 0);     //true  : here 'null' convert to 'zero'.
+
+// ===
+console.log("2" == 2);      //true
+console.log("2" === 2);     //false : also check datatype.
+
+
+
+
+
 
 
 
