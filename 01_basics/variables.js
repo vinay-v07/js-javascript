b6e4bc7a445358458eccf3073251b76c90910a56
vinay verma <itxvinayverma@gmail.com>
2025-05-17 03:59:58 +0000

    container config

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "vinay123@gamil.com"
+var accountPwd = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 //not Allowed
+
+accountEmail = "vicky123@gmail.com"
+accountPwd = "54321"
+accountCity ="Raipur"
+
+console.table([accountId,accountEmail,accountPwd,accountCity])
