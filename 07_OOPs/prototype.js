fef93342c477ecc5138f8b2e8a8b8abdf20f8177
vinay verma <itxvinayverma@gmail.com>
2025-05-30 05:27:32 +0000

    Object and Prototypes

--- 07_OOPs/prototype.js
+++ 07_OOPs/prototype.js
@@ -64,37 +64,6 @@ Array.prototype.vicky = function(){
 // myHeros.vicky()
 // heros.vicky()          // error  
 
-// let myName = "hitesh     "
-// let mychannel = "chai     "
-
-// console.log(myName.trueLength);
-
-
-let myHeros = ["thor", "spiderman"]
-
-
-let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
-
-    getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`);
-    }
-}
-
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
-}
-
-Array.prototype.heyHitesh = function(){
-    console.log(`Hitesh says hello`);
-}
-
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
-
 // inheritance
 
 const User = {
